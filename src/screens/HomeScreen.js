1e07b6c28858cf2abd5214ffc33b9b3c2da3caf3
Oğuz Yıldız <iletmoji@gmail.com>
2021-08-26 01:38:55 +0300

    Home has been done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View } from 'react-native';
+import { Card, Button, Icon, Text } from 'react-native-elements';
+import { connect, useDispatch } from 'react-redux';
+import { SIGNOUT } from '../store/actions/types';
 
-const HomeScreen = () => (
-  <View>
-    <Text>Homepage</Text>
-  </View>
-);
+const HomeScreen = ({ user }) => {
+  const dispatch = useDispatch();
 
-export default HomeScreen;
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 5, justifyContent: 'center' }}>
+        <Card>
+          <Card.Title h3 h3Style={{ textAlign: 'left' }}>
+            Welcome, {user.name}
+          </Card.Title>
+          <Text>Full Name: {user.name}</Text>
+          <Text>Email: {user.email}</Text>
+        </Card>
+      </View>
+      <View style={{ flex: 1, padding: 20, justifyContent: 'flex-end' }}>
+        <Button
+          onPress={() => dispatch({ type: SIGNOUT })}
+          title="Sign Out"
+          type="outline"
+        />
+      </View>
+    </View>
+  );
+};
+
+const mapStateToProps = state => ({
+  user: state.user.user,
+});
+
+export default connect(mapStateToProps)(HomeScreen);
